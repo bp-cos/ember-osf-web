cd3d6f3361d7a031b13349d4c5868a8c584befac
Brian Pilati <brianpilati@cos.io>
2024-01-16 10:02:57 -0700

    Added visualization for draft metadata

--- app/guid-node/metadata/index/route.ts
+++ app/guid-node/metadata/index/route.ts
@@ -25,6 +25,7 @@ export default class MetadataRoute extends Route {
         return {
             guidNode: params.guidNode,
             cedarMetadataRecords,
+            defaultIndex: 0,
         };
     }
 }
